b36baf305e039e9957f096b0a25fef1cda50e20b
Daeseong Jeon <jun65800@unist.ac.kr>
2017-10-22 17:45:01 +0000

    login update

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :confirmable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@unist\.ac\.kr\z/, message: "유니스트 이메일을 입력해주세요." }
 end
