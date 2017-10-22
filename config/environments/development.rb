9b8eba19bbf71b1c1a42ba1b30b4f89d475a71c3
Daeseong Jeon <jun65800@unist.ac.kr>
2017-10-22 09:15:01 +0000

    email confirmation update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -38,4 +38,20 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  # For email confirmation
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.default_url_options = { host: '13.124.236.161' }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      user_name:      'nero96in@gmail.com',
+      password:       'wjseotjd0.',
+      domain:         '13.124.236.161',
+      address:       'smtp.gmail.com',
+      port:          '465',
+      authentication: :plain,
+      enable_starttls_auto: true,
+      ssl: true
+    }
 end
