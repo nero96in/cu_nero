850d384061daa3957637e6dbc452c58154410948
Daeseong Jeon <jun65800@unist.ac.kr>
2017-10-23 17:47:15 +0000

    login panel complete but must add role system to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171022042814) do
+ActiveRecord::Schema.define(version: 20171023161246) do
 
   create_table "documents", force: :cascade do |t|
     t.string   "title"
@@ -100,6 +100,8 @@ ActiveRecord::Schema.define(version: 20171022042814) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "club"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
